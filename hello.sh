f3f49c6e59faa868a8f7596dc714d30d016ac440
Aditya Patel <adityasingh2013@gmail.com>
2018-08-01 18:22:08 +0530

    This is the first script 'Hello World'

---
+++ hello.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+##############################################
+#Purpos : To learn how to write the shell script
+#Version: 1.0
+#Owner	: Aditya P <adityapatel081993@gmail.com>
+#Input	: N/A
+#Output	: message will print
+###############################################
+
+echo "Hello world !!"
+echo "Knowledge is power"
