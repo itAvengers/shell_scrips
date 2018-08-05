a80d1e96245bfc0f62404e1c31dd294ca5dc4cd8
Aditya Patel <adityasingh2013@gmail.com>
2018-08-05 19:32:27 +0530

    copy files to the pendrive v.2

--- chapter_2/ans_10.sh
+++ chapter_2/ans_10.sh
@@ -3,7 +3,7 @@
 ##############################################
 #Purpos : Write a shell script that allows a user to enter three file names. 
 #	  The program should then copy all files to USB pen.
-#Version: 1.0
+#Version: 2.0
 #Owner	: Aditya P <adityapatel081993@gmail.com>
 #Input	: file name
 #Output	: file to be copied on Pen Drive
@@ -14,4 +14,17 @@ PEN="/media/usb"
 
 read -p "Enter three file names : " f1 f2 f3
 
-cp -v "$f1" "$f2" "$f3" $PEN
+# Make sure pendrive exist else die
+[ ! -d "$PEN" ] && { echo "$0: USB Pendrive not found at ${PEN}"; exit 1; }
+
+# Make sure pendrive is mounted else die
+
+if grep -wq "$PEN" /etc/mtab
+then
+	# Make copy only if file exist else die
+	[ -f "$f1" ] && cp -v "$f1" $PEN || echo "$0: $f1 not found."
+	[ -f "$f2" ] && cp -v "$f2" $PEN || echo "$0: $f2 not found."
+	[ -f "$f3" ] && cp -v "$f3" $PEN || echo "$0: $f3 not found."
+else
+	echo "$0: USB Pen is not mounted at $PEN."
+fi
